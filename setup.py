63f5a29e8ffab48f32410d66d00852e9e33a8e82
gughan <gugu.ap900@gmail.com>
2022-05-25 16:54:40 +0530

    Restructred based python library terms

--- setup.py
+++ setup.py
@@ -4,12 +4,19 @@ from setuptools import setup
 
 setup(
     name='morcecodetranslator',
-    version='1.0.0',
+    version='1.0.1',
     license='MIT',
     author='Gughanathan M',
     author_email='gugu.ap900@gmail.com',
-    packages=find_packages(),
+    description='Convert text to Morce Code',
+    package_dir={'':'src'},
+    packages=find_packages(where='src'),
     url='https://github.com/gugupy/morcecodetranslator',
     include_package_data=True,
-    keywords='Morce Code'
+    keywords='Morce Code',
+    python_requires=">=3.6",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent"
+    ]
 )
