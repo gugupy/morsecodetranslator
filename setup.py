15dc56b5800978d5ae484664d0276c1743d089ad
gughan <gugu.ap900@gmail.com>
2022-05-25 15:49:09 +0530

    Updated setup config

--- setup.py
+++ setup.py
@@ -5,7 +5,11 @@ from setuptools import setup
 setup(
     name='morcecodetranslator',
     version='1.0.0',
+    license='MIT',
+    author='Gughanathan M',
+    author_email='gugu.ap900@gmail.com',
     packages=find_packages(),
+    url='https://github.com/gugupy/morcecodetranslator',
     include_package_data=True,
-    zip_safe=False
+    keywords='Morce Code'
 )
